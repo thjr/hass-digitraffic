9735ead755098b424075681e8482b091bb176fa6
thjr <teijo.ronka@iki.fi>
2019-11-30 20:58:48 +0200

    remove autoconfiguration for faster startup

--- src/main/java/fi/digitraffic/App.java
+++ src/main/java/fi/digitraffic/App.java
@@ -1,9 +1,13 @@
 package fi.digitraffic;
 
+import fi.digitraffic.hass.SensorValueService;
+import fi.digitraffic.mqtt.MqttService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;                                                                   
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import({MqttService.class, SensorValueService.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
