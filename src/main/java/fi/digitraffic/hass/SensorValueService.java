2ae61a6ca13dc1edb2167f38fa6da26b237e2565
thjr <teijo.ronka@iki.fi>
2019-12-03 16:30:10 +0200

    fix token parameter add unit_of_measurement

--- src/main/java/fi/digitraffic/hass/SensorValueService.java
+++ src/main/java/fi/digitraffic/hass/SensorValueService.java
@@ -4,39 +4,41 @@ import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class SensorValueService {
-    private static final String HASS_ADDRESS = "hassio/homeassistant";
+    private static final String HASSIO_ADDRESS = "hassio/homeassistant";
     private static final Logger LOG = LoggerFactory.getLogger(SensorValueService.class);
 
-    private final String hassToken;
+    private final String hassioToken;
     private final Gson gson = new Gson();
 
-    public SensorValueService(final Environment env) {
-        this.hassToken = env.getProperty("HASS_TOKEN");
-
-        LOG.info("hasstoken {}", hassToken);
+    public SensorValueService(@Value("${HASSIO_TOKEN}") final String token) {
+        this.hassioToken = token;
     }
 
-    public int postSensorValue(final String sensorName, final String value) throws IOException {
-        final URL url = new URL(String.format("http://%s/api/states/sensor.%s", HASS_ADDRESS, sensorName));
-        final HassStateData data = new HassStateData(value, Collections.emptyList());
+    public int postSensorValue(final String sensorName, final String value, final String unitOfMeasurement) throws IOException {
+        final URL url = new URL(String.format("http://%s/api/states/sensor.%s", HASSIO_ADDRESS, sensorName));
+        final Map attributes = new HashMap();
+        final HassStateData data = new HassStateData(value, attributes);
+        attributes.put("unit_of_measurement", unitOfMeasurement);
+
         final HttpURLConnection con = (HttpURLConnection) url.openConnection();
         final String message = gson.toJson(data);
 
         LOG.info("posting {} to {}", message, url.getPath());
 
         con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("X-HA-Access", hassToken);
+        con.setRequestProperty("X-HA-Access", hassioToken);
         con.setRequestMethod("POST");
         con.setDoOutput(true);
         con.getOutputStream().write(message.getBytes());
@@ -47,9 +49,9 @@ public class SensorValueService {
 
     private static class HassStateData {
         final Object state;
-        final List<String> attributes;
+        final Map<String, String> attributes;
 
-        private HassStateData(Object state, List<String> attributes) {
+        private HassStateData(final Object state, final Map<String, String> attributes) {
             this.state = state;
             this.attributes = attributes;
         }
