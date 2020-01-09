fa3b5c821199ac1d1778af424ef3954d7e97210e
thjr <teijo.ronka@iki.fi>
2020-01-09 19:05:22 +0200

    better valuecache

--- src/main/java/fi/digitraffic/mqtt/SensorValueCache.java
+++ src/main/java/fi/digitraffic/mqtt/SensorValueCache.java
@@ -1,5 +1,6 @@
 package fi.digitraffic.mqtt;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -7,10 +8,13 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SensorValueCache {
     private final Map<String, CacheValue> map = new ConcurrentHashMap<>();
 
+    private static final Duration MAX_TIME_BETWEEN = Duration.ofMinutes(1);
+    private static final Duration MIN_TIME_BETWEEN = Duration.ofSeconds(60);
+
     public synchronized boolean checkTimeAndValue(final String key, final String value) {
         final CacheValue cv = map.get(key);
 
-        if(cv == null || cv.timestamp.isBefore(ZonedDateTime.now().minusMinutes(1)) || !cv.value.equals(value)) {
+        if(cv == null || isMaxTime(cv) || hasValueChanged(cv, value)) {
             map.put(key, new CacheValue(value, ZonedDateTime.now()));
 
             return true;
@@ -19,10 +23,18 @@ public class SensorValueCache {
         return false;
     }
 
+    private boolean hasValueChanged(final CacheValue cv, final String value) {
+        return !cv.value.equals(value) && cv.timestamp.isBefore(ZonedDateTime.now().minus(MIN_TIME_BETWEEN));
+    }
+
+    private boolean isMaxTime(final CacheValue cv) {
+        return cv.timestamp.isBefore(ZonedDateTime.now().minus(MAX_TIME_BETWEEN));
+    }
+
     public synchronized boolean checkTime(final String key) {
         final CacheValue cv = map.get(key);
 
-        if(cv == null || cv.timestamp.isBefore(ZonedDateTime.now().minusMinutes(1))) {
+        if(cv == null || isMaxTime(cv)) {
             map.put(key, new CacheValue(null, ZonedDateTime.now()));
 
             return true;
